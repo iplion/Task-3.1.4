aff8acac10cde67b81e135cf4495ca29804d8be8
Lev Evseev <lionevseev@gmail.com>
2025-05-15 13:46:10 +0700

    resolve#0

---
+++ src/main/java/com/iplion/task314/model/User.java
@@ -0,0 +1,12 @@
+package com.iplion.task314.model;
+
+import lombok.*;
+
+@Data
+public class User
+
+{   private Long id;
+    private String name;
+    private String lastName;
+    private Integer age;
+}
